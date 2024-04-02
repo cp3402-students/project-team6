632b3a2cd38e57994e39a5d3ea9bdc3147c18086
LeeCrawford11 <80378138+LeeCrawford11@users.noreply.github.com>
2024-04-02 22:32:26 +1000

    front page template

---
+++ wp-content/themes/underscores/sidebar-sticky.php
@@ -0,0 +1,15 @@
+<aside id="sticky-sidebar" class="widget-area">
+    <section class="widget">
+        <?php get_search_form(); ?>
+    </section>
+    <section class="widget">
+        <!-- Placeholder for login. Adjust as needed. -->
+        <h2>Login</h2>
+        <a href="<?php echo wp_login_url(); ?>">Login</a>
+    </section>
+    <section class="widget">
+        <!-- Placeholder for sign up. Adjust as needed. -->
+        <h2>Sign Up</h2>
+        <a href="<?php echo wp_registration_url(); ?>">Sign Up</a>
+    </section>
+</aside>
