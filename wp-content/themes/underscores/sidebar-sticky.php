7235ecb39e3578c76eb6d1a3f339adf7df9d06bb
LeeCrawford11 <80378138+LeeCrawford11@users.noreply.github.com>
2024-04-08 14:56:30 +1000

    changed sidebar links to buttons and made "sticky" function work more effectively

--- wp-content/themes/underscores/sidebar-sticky.php
+++ wp-content/themes/underscores/sidebar-sticky.php
@@ -1,17 +1,18 @@
 <aside id="sticky-sidebar" class="widget-area">
 	<?php if ( is_user_logged_in() ) : ?>
         <section class="widget">
-            <h2>Welcome, <?php echo wp_get_current_user()->display_name ?>!</h2>
-            <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Our Courses' ) ) ); ?>">Go to Courses</a>
-            <a href="<?php echo wp_logout_url( get_permalink() ); ?>">Log Out</a>
+            <h2>Welcome, <?php echo wp_get_current_user()->display_name; ?>!</h2>
+            <!-- Using buttons for actions -->
+            <button onclick="window.location.href='<?php echo esc_url( get_permalink( get_page_by_title( 'Our Courses' ) ) ); ?>'">Go to Courses</button>
+            <button onclick="window.location.href='<?php echo esc_url( wp_logout_url( home_url() ) ); ?>'">Log Out</button>
         </section>
 	<?php else : ?>
         <section class="widget">
-            <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Login' ) ) ); ?>">Login</a> |
+            <button onclick="window.location.href='<?php echo esc_url( wp_login_url( get_permalink() ) ); ?>'">Log In</button>
         </section>
 	<?php endif; ?>
     <section class="widget">
         <h2>Sign Up</h2>
-        <a href="<?php echo 'http://localhost:8080/?page_id=116'; ?>">Sign Up</a>
+        <button onclick="window.location.href='<?php echo esc_url( home_url('/?page_id=116') ); ?>'">Sign Up</button>
     </section>
 </aside>
