bd7de48f493c952fa74ad527ba825ee5b92204d8
LeeCrawford11 <80378138+LeeCrawford11@users.noreply.github.com>
2024-04-12 14:07:53 +1000

    Added just the theme to repo

---
+++ underscores/footer.php
@@ -0,0 +1,15 @@
+<footer id="colophon" class="site-footer">
+    <div class="footer-widgets">
+        <div class="footer-widget-area">
+            <?php if(is_active_sidebar('footer-1')): ?>
+                <?php dynamic_sidebar('footer-1'); ?>
+            <?php endif; ?>
+        </div>
+        <div class="footer-widget-area">
+            <?php if(is_active_sidebar('footer-2')): ?>
+                <?php dynamic_sidebar('footer-2'); ?>
+            <?php endif; ?>
+        </div>
+    </div><!-- .footer-widgets -->
+</footer><!-- #colophon -->
+<?php wp_footer(); ?>
